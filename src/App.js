bead3c80d2cfd7e49d0dc874cc4bf41d66e68c48
faborubio <fabian.rubiocs@gmail.com>
2023-06-12 12:07:32 -0400

    add firebase config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './components/Home'
+import About from './components/About'
+import Skills from './components/Skills'
+import Contact from './components/Contact'
+import Portafolio from './components/Portafolio'
+import './App.scss'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portafolio" element={<Portafolio />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
